5ad98a357b6f8db009c22b520e4b9f588dc415dc
Rodrigo Ortega Cuesta <rodortcue@gmail.com>
2016-03-08 23:59:32 +0100

    Alpha stage-3

--- README.md
+++ README.md
@@ -1,9 +1,33 @@
 # slush-react-dumb-component
 
-Suite of [React](https://facebook.github.io/react/) generators for
-[slush](http://slushjs.github.io).
+> Suite of [React](https://facebook.github.io/react/) generators for [slush](http://slushjs.github.io).
 
-**Under Development (WIP)**
+## Getting Started
+
+Install `slush` and `slush-react-generators` globally:
+
+```bash
+npm i -g slush
+npm i -g slush-react-generators
+```
+
+### Generate a React component module:
+
+Create a new folder for your component and run the slush generator within it:
+
+```bash
+mkdir your-component-name && cd your-component-name
+slush react-generators:component
+```
+
+### Generate a React + Redux app:
+
+Create a new folder for your app and run the slush generator within it:
+
+```bash
+mkdir your-app-name && cd your-app-name
+slush react-generators:app
+```
 
 ### License
 react-dumb-component is available under MIT. See LICENSE for more details.
---
+++ generators/app.js
@@ -0,0 +1,66 @@
+'use strict';
+
+var _ = require('lodash');
+var conflict = require('gulp-conflict');
+var gulp = require('gulp');
+var inquirer = require('inquirer');
+var install = require('gulp-install');
+var rename = require('gulp-rename');
+var template = require('gulp-template');
+
+module.exports = function(done) {
+  var prompts = [
+    {
+      name: 'name',
+      message: 'What would you like to call it? (example: ReactApp)',
+    },
+    {
+      name: 'description',
+      message: 'How would you describe it?',
+    },
+    {
+      name: 'keywords',
+      message: 'How would you describe it in comma seperated key words?',
+    },
+    {
+      name: 'author',
+      message: 'What is your company/author name?',
+    },
+    {
+      name: 'email',
+      message: 'What is your company/author email?',
+    },
+    {
+      type: 'confirm',
+      name: 'moveon',
+      message: 'Continue?',
+    }
+  ];
+
+  //Ask
+  inquirer.prompt(prompts, function (answers) {
+    if (!answers.moveon) { return done(); }
+
+    answers.slugifiedName = _.kebabCase(answers.name);
+    answers.capitalizedAuthor = _.capitalize(answers.author);
+
+    gulp.src(__dirname + '/../templates/app/**/*')
+      .pipe(template(answers, {interpolate: /<\?\?(.+?)\?>/g}))
+      .pipe(rename(function (file) {
+        console.log(file);
+        if (file.basename.indexOf('_') === 0) {
+          file.basename = file.basename.replace('_', '.');
+        }
+        if (file.dirname.indexOf('slush_name') > -1 || file.basename.indexOf('slush_name') > -1
+            || file.extname.indexOf('slush_name') > -1) {
+          file.dirname = file.dirname.replace('slush_name', answers.name);
+          file.basename = file.basename.replace('slush_name', answers.name);
+          file.extname = file.extname.replace('slush_name', answers.name);
+        }
+      }))
+      .pipe(conflict('./'))
+      .pipe(gulp.dest('./'))
+      .pipe(install())
+      .on('end', function() { done(); });
+  });
+};

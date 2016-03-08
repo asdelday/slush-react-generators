1fa0b3bfb34e5d428f450bd5e4471d7c07a29a2c
Rodrigo Ortega Cuesta <rodortcue@gmail.com>
2016-03-09 00:01:41 +0100

    fix slushfile

--- slushfile.js
+++ slushfile.js
@@ -5,3 +5,4 @@ var gulp = require('gulp');
 // load generators
 gulp.task('default', ['component']);
 gulp.task('component', require('./generators/component'));
+gulp.task('app', require('./generators/app'));
